23562d925597b7deb92a13aec1f714aa225a5fd2
Manuel Lorenzo <manulorenzop@gmail.com>
2016-09-24 15:51:29 +0200

    updated user registration and new views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw {
+  devise_for :users, controllers: { registrations: 'registrations' }
   get 'pages/home'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+}
