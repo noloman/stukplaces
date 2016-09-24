5ef9aef1397ecab491149e297532e98ae0dd916e
Manuel Lorenzo <manulorenzop@gmail.com>
2016-09-24 17:47:46 +0200

    scaffolded places and updated views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw {
   devise_for :users, controllers: { registrations: 'registrations' }
   get 'pages/home'
   root 'pages#home'
+  resources :places
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 }
