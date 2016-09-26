801ebe4dc533287dfa05a4408c82170c3854bf21
Manuel Lorenzo <manulorenzop@gmail.com>
2016-09-26 11:41:03 +0200

    removed phone and website mandatory params for places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,6 @@
 class Place < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :address
-  validates_presence_of :phone
-  validates_presence_of :website
   validates_presence_of :user_id
 
   belongs_to :user
